71a177ec4654d9acfe4523d36c8e53f4ae221f4b
Mazev <77510376+Mazev@users.noreply.github.com>
2021-04-04 22:24:09 +0300

    new

---
+++ 05.Classes and Instances/02. Vet.py
@@ -0,0 +1,94 @@
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name, ):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if Vet.space <= len(Vet.animals):
+            return "Not enough space"
+
+        Vet.animals.append(animal_name)
+        self.animals.append(animal_name)
+        return f"{animal_name} registered in the clinic"
+
+    def unregister_animal(self, animals_name) -> object:
+        if animals_name not in self.animals:
+            return f"{animals_name} not in the clinic"
+
+        Vet.animals.remove(animals_name)
+        self.animals.remove(animals_name)
+        return f"{animals_name} unregistered successfully"
+
+    def info(self):
+        vet_animal_space = len(self.animals)
+        clinic_animal_count = len(Vet.animals)
+        space_left = Vet.space - clinic_animal_count
+        return f"{self.name} has {vet_animal_space} animals. {space_left} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
+
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_init(self):
+        vet = Vet("Bob")
+        Vet.animals = []
+        Vet.space = 5
+        self.assertEqual(vet.name, "Bob")
+        self.assertEqual(vet.animals, [])
+        self.assertEqual(Vet.animals, [])
+        self.assertEqual(Vet.space, 5)
+
+    def test_register_successfull(self):
+        vet = Vet("Bob")
+        Vet.animals = []
+        Vet.space = 5
+        vet2 = Vet("Peter")
+        res = vet.register_animal("Doggy")
+        self.assertEqual(res, "Doggy registered in the clinic")
+        self.assertEqual(vet.animals, ["Doggy"])
+        self.assertEqual(vet.animals, ["Doggy"])
+        self.assertEqual(vet2.animals, [])
+
+    def test_register_unsuccessfull(self):
+        vet = Vet("Bob")
+        Vet.animals = []
+        Vet.space = 5
+        for i in range(6):
+            vet.register_animal(str(i))
+        res = vet.register_animal("Doggy")
+        self.assertEqual(res, "Not enough space")
+        self.assertEqual(len(Vet.animals), 5)
+        self.assertEqual(len(vet.animals), 5)
+
+    def test_unregister_successfull(self):
+        vet = Vet("Bob")
+        Vet.animals = []
+        Vet.space = 5
+        vet.register_animal("Kitty")
+        res = vet.unregister_animal("Kitty")
+        self.assertEqual(res, "Kitty unregistered successfully")
+        self.assertEqual(vet.animals, [])
+        self.assertEqual(Vet.animals, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
