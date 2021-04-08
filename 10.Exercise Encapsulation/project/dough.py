5062596283d7d00ba59f05fb1892b63004c5b05c
Mazev <77510376+Mazev@users.noreply.github.com>
2021-04-09 01:30:09 +0300

    new

---
+++ 10.Exercise Encapsulation/project/dough.py
@@ -0,0 +1,32 @@
+class Dough:
+    def __init__(self,flour_type, baking_technique, weight):
+        self.__flour_type = flour_type
+        self.__baking_technique = baking_technique
+        self.__weight = weight
+        
+        
+    @property
+    def flour_type(self):
+        return 
+    
+    @flour_type.setter
+    def flour_type(self, value):
+        pass
+    
+    
+    @property
+    def baking_technique(self):
+        return 
+    
+    @baking_technique.setter
+    def baking_technique(self, value):
+        pass
+    
+    
+    @property
+    def weight(self):
+        return 
+    
+    @weight.setter
+    def weight(self, value):
+        pass
