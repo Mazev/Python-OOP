10558dec70170ab3fee1ecb931365e49489f19be
Mazev <77510376+Mazev@users.noreply.github.com>
2021-04-09 22:50:22 +0300

    new

---
+++ 11.Inheritance/demo.py
@@ -0,0 +1,28 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+    def __repr__(self):
+        return f"{self.name}: {self.age}"
+
+def get_object_by_attr_value(objects, **kwargs):
+    result = []
+    for obj in objects:
+        is_valid = True
+        for key, value in kwargs.items():
+            if getattr(obj, key, None) != value:
+                is_valid = False
+                break
+
+        if is_valid:
+            result.append(obj)
+    return result
+
+people = [
+    Person("Pesho", 12),
+    Person("Gosho", 10)
+]
+print(get_object_by_attr_value(people, age = 12))
