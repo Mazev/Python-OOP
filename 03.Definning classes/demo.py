a8fddd359bf35c2acd0869d7d4e612535eea76dc
Mazev <77510376+Mazev@users.noreply.github.com>
2021-04-12 18:32:08 +0300

    new

--- 03.Definning classes/demo.py
+++ 03.Definning classes/demo.py
@@ -79,25 +79,45 @@
 # print(cup.status())
 
 
-class Flower:
-    def __init__(self, name, water_requirements):
-        self.name = name
-        self.water_requirements = water_requirements
-        self.is_happy = False
+# class Flower:
+#     def __init__(self, name, water_requirements):
+#         self.name = name
+#         self.water_requirements = water_requirements
+#         self.is_happy = False
+#
+#     def water(self, quantity):
+#         if quantity >= self.water_requirements:
+#             self.is_happy = True
+#
+#     def status(self):
+#         if self.is_happy:
+#             return f"{self.name} is happy"
+#
+#         return f"{self.name} is not happy"
+#
+#
+# flower = Flower("Lilly", 100)
+# flower.water(50)
+# print(flower.status())
+# flower.water(100)
+# print(flower.status())
 
-    def water(self, quantity):
-        if quantity >= self.water_requirements:
-            self.is_happy = True
-
-    def status(self):
-        if self.is_happy:
-            return f"{self.name} is happy"
-
-        return f"{self.name} is not happy"
 
 
-flower = Flower("Lilly", 100)
-flower.water(50)
-print(flower.status())
-flower.water(100)
-print(flower.status())
+# class Car:
+#     def __init__(self, name, model, engine):
+#         self.name= name
+#         self.model = model
+#         self.engine = engine
+#
+#     def get_info(self):
+#         return f"This is {self.name} {self.model} with engine {self.engine}"
+#
+#
+#
+#
+# car = Car("Kia", "Rio", "1.3L B3 I4")
+# print(car.get_info())
+
+
+
